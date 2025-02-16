66f1d5c69d488fcdb518df0648a9d7dadc1138a8
Jamin <jamin.sk@yahoo.com>
2025-02-16 12:10:02 +0100

    better db model

--- jam_man/db.py
+++ jam_man/db.py
@@ -3,8 +3,10 @@ from jam_man import db
 class Song(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
-    artist = db.Column(db.String(100), nullable=False)
-    file_path = db.Column(db.String(500), nullable=False)
+    artist = db.Column(db.String(200), nullable=True)
+    album = db.Column(db.String(200), nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    file_path = db.Column(db.String(200), nullable=False)
 
 def init_db():
     with db.app.app_context():
